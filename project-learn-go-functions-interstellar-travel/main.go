4c2fb9f9ec074f49640347e3b37e9f194cd8e86e
ARBUCHELI <radio_seattle@hotmail.com>
2024-01-03 13:45:08 +0300

    project point 7

---
+++ project-learn-go-functions-interstellar-travel/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Create the function fuelGauge() here
+func fuelGauge(fuel int) {
+  fmt.Println("You have", fuel, "gallons of fuel left!")
+}
+
+// Create the function calculateFuel() here
+func calculateFuel(planet string) int {
+  
+}
+
+// Create the function greetPlanet() here
+
+
+// Create the function cantFly() here
+
+
+// Create the function flyToPlanet() here
+
+
+func main() {
+  // Test your functions!
+  fuelGauge(140)
+  // Create `planetChoice` and `fuel`
+  
+  // And then liftoff!
+  
+}
